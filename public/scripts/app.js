52f1603c4430e6bb946d229e03ffe95924bd51c9
BUDDHAPLEX <ericandrade@gmail.com>
2018-04-07 21:07:57 -0400

    configuration

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,13 @@
 'use strict';
+
 console.log('App.js is running');
+
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1><p>This is some info.</p>;
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
