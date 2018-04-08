8a09fbf42f89b608063c979125bd086ef458d408
BUDDHAPLEX <ericandrade@gmail.com>
2018-04-07 22:56:59 -0400

    saving with some minor updates

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,38 +2,73 @@
 
 console.log('App.js is running');
 
+// template
+
 var title = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer.'
+	title: 'Indecision App',
+	subtitle: 'Put your life in the hands of a computer.',
+	options: ['One', 'Two']
 };
+
 var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        title.title
-    ),
-    React.createElement(
-        'p',
-        null,
-        title.subtitle
-    )
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		title.title
+	),
+	title.subtitle && React.createElement(
+		'p',
+		null,
+		title.subtitle
+	),
+	React.createElement(
+		'p',
+		null,
+		title.options.length > 0 ? 'Here are your options.' : 'You have no options.'
+	)
 );
 
-// var user = {
-// 	name: 'Eric',
-// 	age: 45,
-// 	location: 'Edison, NJ'
-// };
-// var templateTwo = (
-// 	<div>
-// 		<h1>{user.name}</h1>
-// 		<p><b>Age:</b> {user.age}</p>
-// 		<p><b>Location:</b> {user.location}</p>
-// 	</div>
-// );
+// templateTwo
+
+var user = {
+	name: 'Eric Andrade',
+	age: 46,
+	location: 'Edison, NJ'
+};
+
+function getLocation(location) {
+	if (location) {
+		return React.createElement(
+			'p',
+			null,
+			'Location: ',
+			location
+		);
+	}
+}
+
+var templateTwo = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		user.name ? user.name : 'Anonymous'
+	),
+	user.age && user.age >= 18 && React.createElement(
+		'p',
+		null,
+		'Age: ',
+		user.age
+	),
+	getLocation(user.location)
+);
 
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+
+// EXTRA CODE
+// <p><b>Age:</b> {user.age}</p>
--- src/app.js
+++ src/app.js
@@ -1,29 +1,46 @@
 console.log('App.js is running');
 
+// template
+
 var title = {
 	title: 'Indecision App',
-	subtitle: 'Put your life in the hands of a computer.'
-}
+	subtitle: 'Put your life in the hands of a computer.',
+	options: ['One', 'Two']
+};
+
 var template = (
-    <div>
-        <h1>{title.title}</h1>
-        <p>{title.subtitle}</p>
-    </div>
+	<div>
+		<h1>{title.title}</h1>
+		{title.subtitle && <p>{title.subtitle}</p>}
+		<p>{title.options.length > 0 ? 'Here are your options.' : 'You have no options.'}</p>
+	</div>
 );
 
-// var user = {
-// 	name: 'Eric',
-// 	age: 45,
-// 	location: 'Edison, NJ'
-// };
-// var templateTwo = (
-// 	<div>
-// 		<h1>{user.name}</h1>
-// 		<p><b>Age:</b> {user.age}</p>
-// 		<p><b>Location:</b> {user.location}</p>
-// 	</div>
-// );
+// templateTwo
+
+var user = {
+	name: 'Eric Andrade',
+	age: 46,
+	location: 'Edison, NJ'
+};
+
+function getLocation(location) {
+	if (location) {
+		return <p>Location: {location}</p>;
+	} 
+}
+
+var templateTwo = (
+	<div>
+		<h1>{user.name ? user.name : 'Anonymous'}</h1>
+		{(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+		{getLocation(user.location)}
+	</div>
+);
 
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+
+// EXTRA CODE
+// <p><b>Age:</b> {user.age}</p>
