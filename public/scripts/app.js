6bd1adf266a605595c5e5bd9eebbc4cda588f6fc
BUDDHAPLEX <ericandrade@gmail.com>
2018-04-08 19:16:33 -0400

    more practics

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,30 @@
 'use strict';
 
-console.log('App.js is running');
+console.log('App.js is running...yo!');
 
 // template
 
-var title = {
+var app = {
 	title: 'Indecision App',
 	subtitle: 'Put your life in the hands of a computer.',
-	options: ['One', 'Two']
+	options: []
 };
+
+var onFormSubmit = function onFormSubmit(e) {
+	e.preventDefault();
+
+	var option = e.target.elements.option.value;
+
+	if (option) {
+		app.options.push(option);
+		e.target.elements.option.value = ' ';
+	}
+};
+
+// renderOptionsLength () => {
+// 	render(app.options.length);
+
+// }
 
 var template = React.createElement(
 	'div',
@@ -16,19 +32,53 @@ var template = React.createElement(
 	React.createElement(
 		'h1',
 		null,
-		title.title
+		app.title
 	),
-	title.subtitle && React.createElement(
+	app.subtitle && React.createElement(
 		'p',
 		null,
-		title.subtitle
+		app.subtitle
 	),
 	React.createElement(
 		'p',
 		null,
-		title.options.length > 0 ? 'Here are your options.' : 'You have no options.'
+		app.options.length > 0 ? 'Here are your options.' : 'You have no options.'
+	),
+	React.createElement(
+		'p',
+		null,
+		app.options.length
+	),
+	React.createElement(
+		'ol',
+		null,
+		React.createElement(
+			'li',
+			null,
+			'Item one'
+		),
+		React.createElement(
+			'li',
+			null,
+			'Item two'
+		)
+	),
+	React.createElement(
+		'form',
+		{ onSubmit: onFormSubmit },
+		React.createElement('input', { type: 'text', name: 'option' }),
+		React.createElement(
+			'button',
+			null,
+			'Add Option'
+		)
 	)
 );
 
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
+render();
+
+// create render functin that renders new jsx
+// call to initialize
+// then call after options array is added to it.
--- src/app.js
+++ src/app.js
@@ -1,20 +1,53 @@
-console.log('App.js is running');
+console.log('App.js is running...yo!');
 
 // template
 
-const title = {
+const app = {
 	title: 'Indecision App',
 	subtitle: 'Put your life in the hands of a computer.',
-	options: ['One', 'Two']
+	options: []
 };
 
-const template = (
-	<div>
-		<h1>{title.title}</h1>
-		{title.subtitle && <p>{title.subtitle}</p>}
-		<p>{title.options.length > 0 ? 'Here are your options.' : 'You have no options.'}</p>
-	</div>
-);
+const onFormSubmit = (e) => {
+	e.preventDefault();
+
+	const option = e.target.elements.option.value;
+
+	if (option) {
+		app.options.push(option);
+		e.target.elements.option.value = ' ';
+	}
+};
+
+// renderOptionsLength () => {
+// 	render(app.options.length);
+	
+// }
+
+	const template = (
+		<div>
+			<h1>{app.title}</h1>
+			{app.subtitle && <p>{app.subtitle}</p>}
+			<p>{app.options.length > 0 ? 'Here are your options.' : 'You have no options.'}</p>
+			<p>{app.options.length}</p>
+			<ol>
+				<li>Item one</li>
+				<li>Item two</li>
+			</ol>
+			<form onSubmit={onFormSubmit}>
+				<input type="text" name="option"/>
+				<button>Add Option</button>
+			</form>
+		</div>
+
+
+	);
+
 
 const appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
+render()
+
+// create render functin that renders new jsx
+// call to initialize
+// then call after options array is added to it.
