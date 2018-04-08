3a499fdd68e4f960c043f42cf40648ba9f2ca9cb
BUDDHAPLEX <ericandrade@gmail.com>
2018-04-07 21:39:37 -0400

    initializing files.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,38 @@
 
 console.log('App.js is running');
 
-// JSX - JavaScript XML
+var title = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer.'
+};
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        title.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        title.subtitle
+    )
 );
+
+// var user = {
+// 	name: 'Eric',
+// 	age: 45,
+// 	location: 'Edison, NJ'
+// };
+// var templateTwo = (
+// 	<div>
+// 		<h1>{user.name}</h1>
+// 		<p><b>Age:</b> {user.age}</p>
+// 		<p><b>Location:</b> {user.location}</p>
+// 	</div>
+// );
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 console.log('App.js is running');
 
-// JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+var title = {
+	title: 'Indecision App',
+	subtitle: 'Put your life in the hands of a computer.'
+}
+var template = (
+    <div>
+        <h1>{title.title}</h1>
+        <p>{title.subtitle}</p>
+    </div>
+);
+
+// var user = {
+// 	name: 'Eric',
+// 	age: 45,
+// 	location: 'Edison, NJ'
+// };
+// var templateTwo = (
+// 	<div>
+// 		<h1>{user.name}</h1>
+// 		<p><b>Age:</b> {user.age}</p>
+// 		<p><b>Location:</b> {user.location}</p>
+// 	</div>
+// );
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
