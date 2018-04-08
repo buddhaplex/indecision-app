9156837d4408771551e2977c67488ed5ecfe3fca
BUDDHAPLEX <ericandrade@gmail.com>
2018-04-08 10:56:53 -0400

    learning, playing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,27 +1,74 @@
 'use strict';
 
-// arguments object no longer bound with arrow functions
+console.log('App.js is running');
 
-var add = function add(a, b) {
-    // console.log(arguments);
-    return a + b;
+// template
+
+var title = {
+	title: 'Indecision App',
+	subtitle: 'Put your life in the hands of a computer.',
+	options: ['One', 'Two']
 };
-console.log(add(55, 1));
 
-// this keyword - no longer bound with arrow functions
+var template = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		title.title
+	),
+	title.subtitle && React.createElement(
+		'p',
+		null,
+		title.subtitle
+	),
+	React.createElement(
+		'p',
+		null,
+		title.options.length > 0 ? 'Here are your options.' : 'You have no options.'
+	)
+);
+
+// templateTwo
 
 var user = {
-    name: 'Eric',
-    cities: ['San Francisco', 'Boston', 'Chicago'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        console.log(this.name);
-        console.log(this.cities);
-
-        this.cities.forEach(function (city) {
-            console.log(_this.name + ' has lived in ' + city);
-        });
-    }
+	name: 'Eric Andrade',
+	age: 46,
+	location: 'Edison, NJ'
 };
-user.printPlacesLived();
+
+function getLocation(location) {
+	if (location) {
+		return React.createElement(
+			'p',
+			null,
+			'Location: ',
+			location
+		);
+	}
+}
+
+var templateTwo = React.createElement(
+	'div',
+	null,
+	React.createElement(
+		'h1',
+		null,
+		user.name ? user.name : 'Anonymous'
+	),
+	user.age && user.age >= 18 && React.createElement(
+		'p',
+		null,
+		'Age: ',
+		user.age
+	),
+	getLocation(user.location)
+);
+
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
+
+// EXTRA CODE
+// <p><b>Age:</b> {user.age}</p>
--- src/playground/es6-arrow-function_2.js
+++ src/playground/es6-arrow-function_2.js
@@ -4,20 +4,28 @@ const add = (a, b) => {
     // console.log(arguments);
     return a + b;
 };
-console.log(add(55,1));
+console.log(add(55, 1));
 
 // this keyword - no longer bound with arrow functions
 
 const user = {
     name: 'Eric',
     cities: ['San Francisco', 'Boston', 'Chicago'],
-    printPlacesLived: function() {
-        console.log(this.name);
-        console.log(this.cities);
-
-        this.cities.forEach((city) => {
-            console.log(this.name + ' has lived in ' + city);
-        });
+    printPlacesLived() {
+        return this.cities.map((city) => this.name + ' has lived in ' + city + '!');
     }
 };
-user.printPlacesLived();
+console.log(user.printPlacesLived());
+
+// object with data on it and a method that accesses the data 
+// use map to traverse it.
+
+const multiplier = {
+    numbers: [3, 4, 5, 5, 7],
+    multiplyBy: 2,
+    multiply() {
+        return this.numbers.map((num) => num * this.multiplyBy);
+    }
+};
+
+console.log(multiplier.multiply(2));
